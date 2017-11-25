3a832da9139fb48c527509d829b510a4a00d4269
Alexey Thaikovsky <liarkajn@gmail.com>
2017-11-25 13:50:44 +0300

    add logger

--- src/main/java/com/likeit/web/dao/connector/Connector.java
+++ src/main/java/com/likeit/web/dao/connector/Connector.java
@@ -5,11 +5,15 @@ import main.java.com.likeit.web.dao.exception.DAOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static main.java.com.likeit.web.dao.exception.Exceptions.CONNECTION_CREATION_EXCEPTION;
+import static main.java.com.likeit.web.dao.exception.Exceptions.MISSED_JDBC_DRIVER;
 
 public class Connector {
 
+    private final static Logger LOGGER = Logger.getLogger(Connector.class.getName());
     private final static String DB_URL = "jdbc:mysql://localhost:3306/like_it";
     private final static String USERNAME = "root";
     private final static String PASSWORD = "root";
@@ -19,7 +23,7 @@ public class Connector {
         try {
             Class.forName(JDBC_DRIVER);
         } catch (ClassNotFoundException e) {
-            //throw new DAOException(MISSED_JDBC_DRIVER.getMessage(), e);
+            LOGGER.log(Level.INFO, MISSED_JDBC_DRIVER.getMessage(), e);
         }
     }
 
